bceefb840bfd185a65534104c3086cf171287f3f
sbx_user1060 <sbx_user1060@ip-10-129-173-35.eu-west-2.compute.internal>
2019-02-27 17:38:47 +0000

    Wed Feb 27 17:38:47 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/DiscountStrategy.java
@@ -0,0 +1,46 @@
+package befaster.solutions.CHK;
+
+import Checkout;
+import java.util.List;
+
+public class DiscountStrategy implements CheckoutStrategy {
+
+@Override
+public void calculateTotal(List<String> items, double[] result) {
+    ItemCounter counter = items.stream().collect(ItemCounter::new, ItemCounter::accept, ItemCounter::combine);
+
+    double appleDiscount = (counter.counts[ItemCounter.APPLE_INDEX] / 2) * Checkout.PRICE_MAP.get("Apple");
+    double orangeDiscount = (counter.counts[ItemCounter.ORANGE_INDEX] / 3) * Checkout.PRICE_MAP.get("Orange");
+
+    result[0] -= orangeDiscount;
+    result[0] -= appleDiscount;
+}
+
+class ItemCounter {
+    static final int APPLE_INDEX = 0;
+    static final int ORANGE_INDEX = 1;
+    private int[] counts = new int[2];
+
+    public void accept(String item) {
+        switch (item) {
+            case "Apple":
+                counts[APPLE_INDEX]++;
+                break;
+
+            case "Orange":
+                counts[ORANGE_INDEX]++;
+                break;
+        }
+    }
+
+    public void combine(ItemCounter other) {
+        this.counts[APPLE_INDEX] += other.getCounts()[APPLE_INDEX];
+        this.counts[APPLE_INDEX] += other.getCounts()[ORANGE_INDEX];
+    }
+
+    public int[] getCounts() {
+        return counts;
+    }
+    }
+
+ }
---
+++ src/main/java/befaster/solutions/CHK/SummingStrategy.java
@@ -0,0 +1,13 @@
+package befaster.solutions.CHK;
+
+import java.util.List;
+import static Checkout.PRICE_MAP;
+
+public class SummingStrategy implements CheckoutStrategy {
+
+@Override
+public void calculateTotal(List<String> items, double[] result) {
+    result[0] = items.stream().mapToDouble(PRICE_MAP::get).sum();
+  }
+
+}
