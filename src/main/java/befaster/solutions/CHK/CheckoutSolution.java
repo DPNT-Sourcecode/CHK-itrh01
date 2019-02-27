2af966d57e3a60df1befb49599cc78949d2b55f8
sbx_user1059 <sbx_user1059@ip-10-129-112-167.eu-west-2.compute.internal>
2019-02-27 19:24:11 +0000

    Wed Feb 27 19:24:11 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -10,13 +10,15 @@ import java.util.Set;
 public class CheckoutSolution {
 	
     public Integer checkout(String skus) {
+    	Checkout pricing = new SimpleCheckout();
     	ArrayList<String> shoppingCart = new ArrayList<>();
     	for(int index=0; index < skus.length(); index++) {
     		shoppingCart.add(skus.substring(index, index+1));
+    		
+    		return 1;
     	}
     	
-    	calculateTotal
-       
-    	return 0;
+    
     }
 }
+
